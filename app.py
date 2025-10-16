282f5c6f1d32788062929f82b2d1d4bbc77484ac
Erik Fernando <143031367+Erickfernand@users.noreply.github.com>
2025-10-16 16:19:31 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/pesquisa", methods=["POST"])
+def pesquisa():
+    termo = request.form["termo"]
+    resultado = f"Você pesquisou por: {termo}. (Aqui aparecerá o conteúdo relacionado)"
+    return render_template("resultado.html", resultado=resultado)
+
+if __name__ == "__main__":
+    app.run(debug=True)
